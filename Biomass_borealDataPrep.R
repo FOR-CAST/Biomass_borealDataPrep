ac17f05dc7b21ce18de2db67a75626797007c25b
Eliot McIntire <eliotmcintire@gmail.com>
2026-06-11 17:10:03 -0700

    Add googledrive to reqdPkgs

--- Biomass_borealDataPrep.R
+++ Biomass_borealDataPrep.R
@@ -21,7 +21,7 @@ defineModule(sim, list(
   ),
   reqdPkgs = list(
     "archive", "assertthat", "cli", "data.table", "dplyr", "fasterize",  "ggplot2", "httr2",
-    "merTools", "plyr", "qs2", "rasterVis", "sf", "terra",
+    "merTools", "plyr", "qs2", "rasterVis", "sf", "terra", "googledrive",
     "reproducible (>= 2.1.0)", "SpaDES.core (>= 2.1.0)", "SpaDES.tools (>= 2.0.0)",
     "PredictiveEcology/LandR@development (>= 1.1.5.9090)",
     "PredictiveEcology/pemisc@development",
